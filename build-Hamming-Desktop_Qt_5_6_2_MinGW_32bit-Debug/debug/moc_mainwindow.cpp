085fadf3182d49e3d3cd4c225bcbbd5a6ec473ce
Looookiee <lukasz_wojtaszek@tlen.pl>
2017-05-29 01:35:40 +0200

    Małe zmiany

--- build-Hamming-Desktop_Qt_5_6_2_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
+++ build-Hamming-Desktop_Qt_5_6_2_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[6];
-    char stringdata0[69];
+    QByteArrayData data[5];
+    char stringdata0[75];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -30,15 +30,14 @@ struct qt_meta_stringdata_MainWindow_t {
 static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
 QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
-QT_MOC_LITERAL(1, 11, 19), // "on_kuti_sliderMoved"
-QT_MOC_LITERAL(2, 31, 0), // ""
-QT_MOC_LITERAL(3, 32, 8), // "position"
-QT_MOC_LITERAL(4, 41, 21), // "on_barek_valueChanged"
-QT_MOC_LITERAL(5, 63, 5) // "value"
+QT_MOC_LITERAL(1, 11, 15), // "on_Quit_clicked"
+QT_MOC_LITERAL(2, 27, 0), // ""
+QT_MOC_LITERAL(3, 28, 22), // "on_StartButton_clicked"
+QT_MOC_LITERAL(4, 51, 23) // "on_RandomButton_clicked"
 
     },
-    "MainWindow\0on_kuti_sliderMoved\0\0"
-    "position\0on_barek_valueChanged\0value"
+    "MainWindow\0on_Quit_clicked\0\0"
+    "on_StartButton_clicked\0on_RandomButton_clicked"
 };
 #undef QT_MOC_LITERAL
 
@@ -48,7 +47,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       2,   14, // methods
+       3,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -56,12 +55,14 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   24,    2, 0x08 /* Private */,
-       4,    1,   27,    2, 0x08 /* Private */,
+       1,    0,   29,    2, 0x08 /* Private */,
+       3,    0,   30,    2, 0x08 /* Private */,
+       4,    0,   31,    2, 0x08 /* Private */,
 
  // slots: parameters
-    QMetaType::Void, QMetaType::Int,    3,
-    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -72,10 +73,13 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         MainWindow *_t = static_cast<MainWindow *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->on_kuti_sliderMoved((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 0: _t->on_Quit_clicked(); break;
+        case 1: _t->on_StartButton_clicked(); break;
+        case 2: _t->on_RandomButton_clicked(); break;
         default: ;
         }
     }
+    Q_UNUSED(_a);
 }
 
 const QMetaObject MainWindow::staticMetaObject = {
@@ -103,13 +107,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 2)
+        if (_id < 3)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 2;
+        _id -= 3;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 2)
+        if (_id < 3)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 2;
+        _id -= 3;
     }
     return _id;
 }
--- build-Hamming-Desktop_Qt_5_6_2_MinGW_32bit-Debug/ui_mainwindow.h
+++ build-Hamming-Desktop_Qt_5_6_2_MinGW_32bit-Debug/ui_mainwindow.h
@@ -13,18 +13,16 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
-#include <QtWidgets/QCalendarWidget>
+#include <QtWidgets/QCheckBox>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QLCDNumber>
 #include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
-#include <QtWidgets/QProgressBar>
 #include <QtWidgets/QPushButton>
-#include <QtWidgets/QSlider>
 #include <QtWidgets/QSpinBox>
 #include <QtWidgets/QStatusBar>
-#include <QtWidgets/QTextEdit>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QWidget>
 
@@ -34,17 +32,21 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralWidget;
-    QPushButton *pushButton;
+    QPushButton *StartButton;
     QLabel *label;
     QLabel *label_2;
-    QSpinBox *spinBox;
-    QTextEdit *textEdit;
+    QSpinBox *RandomBoxer;
     QLabel *label_3;
-    QPushButton *pushButton_2;
-    QCalendarWidget *calendarWidget;
+    QPushButton *RandomButton;
+    QLCDNumber *insert;
+    QCheckBox *checkBox;
+    QPushButton *Quit;
+    QSpinBox *spinBox;
+    QLabel *label_4;
     QLCDNumber *lcdNumber;
-    QProgressBar *barek;
-    QSlider *kuti;
+    QLabel *label_5;
+    QLineEdit *PoleTekstowe;
+    QLineEdit *PoleRezultatu;
     QMenuBar *menuBar;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
@@ -63,9 +65,9 @@ public:
         MainWindow->setMaximumSize(QSize(640, 480));
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
-        pushButton = new QPushButton(centralWidget);
-        pushButton->setObjectName(QStringLiteral("pushButton"));
-        pushButton->setGeometry(QRect(10, 160, 131, 51));
+        StartButton = new QPushButton(centralWidget);
+        StartButton->setObjectName(QStringLiteral("StartButton"));
+        StartButton->setGeometry(QRect(60, 230, 71, 31));
         label = new QLabel(centralWidget);
         label->setObjectName(QStringLiteral("label"));
         label->setGeometry(QRect(10, 20, 471, 31));
@@ -74,36 +76,49 @@ public:
         label->setFont(font);
         label_2 = new QLabel(centralWidget);
         label_2->setObjectName(QStringLiteral("label_2"));
-        label_2->setGeometry(QRect(10, 100, 361, 16));
-        spinBox = new QSpinBox(centralWidget);
-        spinBox->setObjectName(QStringLiteral("spinBox"));
-        spinBox->setGeometry(QRect(370, 120, 251, 31));
-        spinBox->setFont(font);
-        textEdit = new QTextEdit(centralWidget);
-        textEdit->setObjectName(QStringLiteral("textEdit"));
-        textEdit->setGeometry(QRect(10, 120, 231, 31));
+        label_2->setGeometry(QRect(10, 170, 171, 16));
+        RandomBoxer = new QSpinBox(centralWidget);
+        RandomBoxer->setObjectName(QStringLiteral("RandomBoxer"));
+        RandomBoxer->setGeometry(QRect(10, 310, 171, 31));
+        RandomBoxer->setFont(font);
         label_3 = new QLabel(centralWidget);
         label_3->setObjectName(QStringLiteral("label_3"));
-        label_3->setGeometry(QRect(370, 100, 251, 20));
-        pushButton_2 = new QPushButton(centralWidget);
-        pushButton_2->setObjectName(QStringLiteral("pushButton_2"));
-        pushButton_2->setGeometry(QRect(370, 160, 131, 51));
-        calendarWidget = new QCalendarWidget(centralWidget);
-        calendarWidget->setObjectName(QStringLiteral("calendarWidget"));
-        calendarWidget->setGeometry(QRect(10, 230, 248, 183));
+        label_3->setGeometry(QRect(10, 290, 171, 20));
+        RandomButton = new QPushButton(centralWidget);
+        RandomButton->setObjectName(QStringLiteral("RandomButton"));
+        RandomButton->setGeometry(QRect(60, 350, 71, 31));
+        insert = new QLCDNumber(centralWidget);
+        insert->setObjectName(QStringLiteral("insert"));
+        insert->setGeometry(QRect(10, 120, 171, 41));
+        insert->setDigitCount(8);
+        insert->setMode(QLCDNumber::Bin);
+        insert->setSegmentStyle(QLCDNumber::Flat);
+        insert->setProperty("intValue", QVariant(248));
+        checkBox = new QCheckBox(centralWidget);
+        checkBox->setObjectName(QStringLiteral("checkBox"));
+        checkBox->setGeometry(QRect(10, 400, 101, 21));
+        Quit = new QPushButton(centralWidget);
+        Quit->setObjectName(QStringLiteral("Quit"));
+        Quit->setGeometry(QRect(550, 410, 75, 23));
+        spinBox = new QSpinBox(centralWidget);
+        spinBox->setObjectName(QStringLiteral("spinBox"));
+        spinBox->setGeometry(QRect(110, 400, 42, 22));
+        label_4 = new QLabel(centralWidget);
+        label_4->setObjectName(QStringLiteral("label_4"));
+        label_4->setGeometry(QRect(250, 200, 131, 131));
+        label_4->setPixmap(QPixmap(QString::fromUtf8("../build-Hamming-Desktop_Qt_5_6_2_MinGW_32bit-Debug/Hamming.png")));
         lcdNumber = new QLCDNumber(centralWidget);
         lcdNumber->setObjectName(QStringLiteral("lcdNumber"));
-        lcdNumber->setGeometry(QRect(370, 230, 231, 91));
-        lcdNumber->setProperty("intValue", QVariant(1763));
-        barek = new QProgressBar(centralWidget);
-        barek->setObjectName(QStringLiteral("barek"));
-        barek->setGeometry(QRect(370, 340, 251, 31));
-        kuti = new QSlider(centralWidget);
-        kuti->setObjectName(QStringLiteral("kuti"));
-        kuti->setGeometry(QRect(370, 390, 221, 19));
-        kuti->setMaximum(100);
-        kuti->setSliderPosition(100);
-        kuti->setOrientation(Qt::Horizontal);
+        lcdNumber->setGeometry(QRect(450, 120, 171, 41));
+        label_5 = new QLabel(centralWidget);
+        label_5->setObjectName(QStringLiteral("label_5"));
+        label_5->setGeometry(QRect(450, 170, 171, 20));
+        PoleTekstowe = new QLineEdit(centralWidget);
+        PoleTekstowe->setObjectName(QStringLiteral("PoleTekstowe"));
+        PoleTekstowe->setGeometry(QRect(10, 190, 171, 31));
+        PoleRezultatu = new QLineEdit(centralWidget);
+        PoleRezultatu->setObjectName(QStringLiteral("PoleRezultatu"));
+        PoleRezultatu->setGeometry(QRect(450, 190, 171, 31));
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
@@ -123,12 +138,16 @@ public:
 
     void retranslateUi(QMainWindow *MainWindow)
     {
-        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", 0));
-        pushButton->setText(QApplication::translate("MainWindow", "START", 0));
-        label->setText(QApplication::translate("MainWindow", "Witaj w programie licz\304\205cym kod Hamminga :)", 0));
-        label_2->setText(QApplication::translate("MainWindow", "Podaj ci\304\205g binarny oraz d\305\202ugo\305\233\304\207 ci\304\205gu", 0));
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "Algorytm Hamminga", 0));
+        StartButton->setText(QApplication::translate("MainWindow", "START", 0));
+        label->setText(QApplication::translate("MainWindow", "Witaj w programie: Algorytm Hamminga", 0));
+        label_2->setText(QApplication::translate("MainWindow", "Podaj ci\304\205g binarny", 0));
         label_3->setText(QApplication::translate("MainWindow", "Albo podaj d\305\202ugo\305\233\304\207 randomowego", 0));
-        pushButton_2->setText(QApplication::translate("MainWindow", "RADOMIUJ!", 0));
+        RandomButton->setText(QApplication::translate("MainWindow", "RANDOM", 0));
+        checkBox->setText(QApplication::translate("MainWindow", "Obecno\305\233\304\207 b\305\202\304\231du", 0));
+        Quit->setText(QApplication::translate("MainWindow", "Wyj\305\233cie", 0));
+        label_4->setText(QString());
+        label_5->setText(QApplication::translate("MainWindow", "Wynik Programu", 0));
     } // retranslateUi
 
 };
